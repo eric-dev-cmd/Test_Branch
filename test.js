603c64c345dfe0fc52628a14be4db9a86052c71b
zunggzing <hello.trungvinh@gmail.com>
2021-08-02 12:51:21 +0700

    test .gitignore

---
+++ test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+test.propTypes = {};
+
+function test(props) {
+  return <div>console.log("test vinh!")</div>;
+}
+
+export default test;
